656b8bd8906e485a38d18c46ce7d8ebd3bb7b532
gatoartstudio <hackhcctecno@gmail.com>
2025-06-12 22:30:49 -0500

    Add: Se agrego logs a la herramienta para descomprimir rars

--- src/helpers/rar_tools.py
+++ src/helpers/rar_tools.py
@@ -1,8 +1,10 @@
 import os
 import rarfile
+import logging
 from typing import Optional
 
 from components.progress_bar_generic import ProgressBarGeneric
+from services.logging_service import config_logging
 
 
 def extract_rar_file(rar_path: str, extract_dir: str, progress_bar: ProgressBarGeneric = None) -> Optional[str]:
@@ -12,7 +14,11 @@ def extract_rar_file(rar_path: str, extract_dir: str, progress_bar: ProgressBarG
     Si se pasa un progress_bar (CTkProgressBar), se actualiza el progreso.
     Devuelve None si todo va bien, o un mensaje de error si falla.
     """
+    config_logging()
+    logging = logging.getLogger()
+
     if not os.path.isfile(rar_path):
+        logging.error(f"El archivo RAR no existe: {rar_path}")
         return f"El archivo RAR no existe: {rar_path}"
     if not os.path.isdir(extract_dir):
         os.makedirs(extract_dir, exist_ok=True)
@@ -28,7 +34,9 @@ def extract_rar_file(rar_path: str, extract_dir: str, progress_bar: ProgressBarG
                     progress_bar.set(idx / total)
                     progress_bar.update_idletasks()
     except rarfile.RarCannotExec as e:
+        logging.error("No se encontró 'unrar' en el sistema. Instálalo para poder extraer archivos RAR.")
         return "No se encontró 'unrar' en el sistema. Instálalo para poder extraer archivos RAR."
     except Exception as e:
+        logging.error(f"Excepción al extraer el archivo RAR: {e}")
         return f"Excepción al extraer: {e}"
     return None
